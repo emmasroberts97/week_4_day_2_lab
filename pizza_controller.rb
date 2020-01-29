7b36d802972fb279c46cdd129bffe2429f9f448f
Emma Roberts <emma.roberts197@gmail.com>
2020-01-29 10:51:28 +0000

    fixed css file

--- pizza_controller.rb
+++ pizza_controller.rb
@@ -1,17 +1,28 @@
-require( 'sinatra' )
-require( 'sinatra/contrib/all' )
-require( 'pry-byebug' )
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
 
 require_relative('./models/pizza_order.rb')
 also_reload('./models/*')
 
-get '/pizza-orders' do #always plural
+get '/pizza-orders' do
   @orders = PizzaOrder.all()
   erb(:index)
 end
 
 get '/pizza-orders/new' do
   erb(:new)
+end
+
+get '/pizza-orders/:id' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+post '/pizza-orders/:id' do
+  @order = PizzaOrder.new(params)
+  @order.update()
+  redirect to('/pizza-orders')
 end
 
 post '/pizza-orders' do
@@ -29,15 +40,4 @@ end
 get '/pizza-orders/:id/edit' do
   @order = PizzaOrder.find(params[:id])
   erb(:edit)
-end
-
-post '/pizza-orders/:id' do
-  @order = PizzaOrder.new(params)
-  @order.update()
-  redirect to('/pizza-orders')
-end
-
-get '/pizza-orders/:id' do
-  @order = PizzaOrder.find(params[:id])
-  erb(:show)
 end
